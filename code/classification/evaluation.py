92fedebf1ad34be0d7285256fb6c3db62e45a2c4
merak <2692195809@qq.com>
2019-04-09 09:01:23 +0800

    使之兼容py2

--- code/classification/evaluation.py
+++ code/classification/evaluation.py
@@ -59,39 +59,39 @@ if __name__ == '__main__':
     np.random.shuffle(bad_test_data)
     np.random.shuffle(good_test_data)
 
-    with print('testing good data'):
-        good_labels = torch.ones(TEST_SIZE).type(torch.LongTensor)
-        good_images = torch.Tensor([])
-        for i in range(TEST_SIZE):
-            image_name = good_test_data[i]
-            image_b = torch.Tensor(
-                cv2.imread(''.join([good_train_data_set_path, IMAGE_B, image_name, end2])))[:, :, 0]
-            image_c = torch.Tensor(
-                cv2.imread(''.join([good_train_data_set_path, IMAGE_C, image_name, end2])))[:, :, 0]
-            image_combine = torch.cat((image_b.unsqueeze(2), image_c.unsqueeze(2)), dim=2)  # 作为二通道的输入
-            good_images = torch.cat((good_images, image_combine.unsqueeze(0)), 0)
-        good_images = good_images.permute((0, 3, 1, 2))
-        good_outputs = resnet18.forward(good_images)
-        predict = (good_outputs[:, 1] > good_outputs[:, 0]).type(torch.LongTensor)
-        accuracy = sum((predict == good_labels).type(torch.FloatTensor)) / len(predict)
-        print(''.join(['Have tested ', str(TEST_SIZE), ' good pictures, accuracy = ', str(accuracy)]))
+    print('testing good data')
+    good_labels = torch.ones(TEST_SIZE).type(torch.LongTensor)
+    good_images = torch.Tensor([])
+    for i in range(TEST_SIZE):
+        image_name = good_test_data[i]
+        image_b = torch.Tensor(
+            cv2.imread(''.join([good_train_data_set_path, IMAGE_B, image_name, end2])))[:, :, 0]
+        image_c = torch.Tensor(
+            cv2.imread(''.join([good_train_data_set_path, IMAGE_C, image_name, end2])))[:, :, 0]
+        image_combine = torch.cat((image_b.unsqueeze(2), image_c.unsqueeze(2)), dim=2)  # 作为二通道的输入
+        good_images = torch.cat((good_images, image_combine.unsqueeze(0)), 0)
+    good_images = good_images.permute((0, 3, 1, 2))
+    good_outputs = resnet18.forward(good_images)
+    predict = (good_outputs[:, 1] > good_outputs[:, 0]).type(torch.LongTensor)
+    accuracy = sum((predict == good_labels).type(torch.FloatTensor)) / len(predict)
+    print(''.join(['Have tested ', str(TEST_SIZE), ' good pictures, accuracy = ', str(accuracy)]))
         
-    with print('testing bad data'):
-        bad_labels = torch.ones(TEST_SIZE).type(torch.LongTensor)
-        bad_images = torch.Tensor([])
-        for i in range(TEST_SIZE):
-            image_name = bad_test_data[i]
-            image_b = torch.Tensor(
-                cv2.imread(''.join([bad_train_data_set_path, image_name[:2], '/', image_name, b, end])))[:, :, 0]
-            image_c = torch.Tensor(
-                cv2.imread(''.join([bad_train_data_set_path, image_name[:2], '/', image_name, c, end])))[:, :, 0]
-            image_combine = torch.cat((image_b.unsqueeze(2), image_c.unsqueeze(2)), dim=2)  # 作为二通道的输入
-            bad_images = torch.cat((bad_images, image_combine.unsqueeze(0)), 0)
-        bad_images = bad_images.permute((0, 3, 1, 2))
-        bad_outputs = resnet18.forward(bad_images)
-        predict = (bad_outputs[:, 1] > bad_outputs[:, 0]).type(torch.LongTensor)
-        accuracy = sum((predict == bad_labels).type(torch.FloatTensor)) / len(predict)
-        print(''.join(['Have tested ', str(TEST_SIZE), ' bad pictures, accuracy = ', str(accuracy)]))
+    print('testing bad data')
+    bad_labels = torch.ones(TEST_SIZE).type(torch.LongTensor)
+    bad_images = torch.Tensor([])
+    for i in range(TEST_SIZE):
+        image_name = bad_test_data[i]
+        image_b = torch.Tensor(
+            cv2.imread(''.join([bad_train_data_set_path, image_name[:2], '/', image_name, b, end])))[:, :, 0]
+        image_c = torch.Tensor(
+            cv2.imread(''.join([bad_train_data_set_path, image_name[:2], '/', image_name, c, end])))[:, :, 0]
+        image_combine = torch.cat((image_b.unsqueeze(2), image_c.unsqueeze(2)), dim=2)  # 作为二通道的输入
+        bad_images = torch.cat((bad_images, image_combine.unsqueeze(0)), 0)
+    bad_images = bad_images.permute((0, 3, 1, 2))
+    bad_outputs = resnet18.forward(bad_images)
+    predict = (bad_outputs[:, 1] > bad_outputs[:, 0]).type(torch.LongTensor)
+    accuracy = sum((predict == bad_labels).type(torch.FloatTensor)) / len(predict)
+    print(''.join(['Have tested ', str(TEST_SIZE), ' bad pictures, accuracy = ', str(accuracy)]))
 
     print('complete testing')
 
